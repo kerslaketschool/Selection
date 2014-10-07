daf5b445f42b1939ba5511fec6b591058ced6c6c
kerslaket <44108@longroad.ac.uk>
2014-10-07 11:10:50 +0100

    Excercise 4

---
+++ Development Excercise 4.py
@@ -0,0 +1,16 @@
+grade = int(input("What is your mark"))
+
+if 80 < grade < 101:
+    print("Your grade is an A")
+if 70 < grade < 81:
+    print("Your grade is an B")
+if 60 < grade < 71:
+    print("Your grade is an C")
+if 50 < grade < 61:
+    print("Your grade is an D")
+if 40 < grade < 51:
+    print("Your grade is an E")
+else:
+    print("Your grade is an U")
+
+    
