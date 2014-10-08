afd417f3c500193cb545f23be5f091de2bf58b78
kerslaket <44108@longroad.ac.uk>
2014-10-08 19:54:57 +0100

    tk

---
+++ Revision task 1.py
@@ -0,0 +1,11 @@
+#Toby Kerslake
+#07-10-2014
+#Revision Excericise 1
+
+number1 = int(input("Please enter your first number"))
+number2 = int(input("Please enter your second number"))
+
+if number1 == number2 :
+    print("The 2 numbers entered are equal")
+else:
+    print("The 2 numbers entered aren't equal")
