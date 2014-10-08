aca3cd5646d976cd0186ef77ffcc2c438fe8d4c6
kerslaket <44108@longroad.ac.uk>
2014-10-08 20:36:58 +0100

    tk

---
+++ Revision task 2.py
@@ -0,0 +1,10 @@
+#Toby Kerslake
+#08-10-2014
+#Revision Excercise 2
+
+age = int(input("What is your age?"))
+
+if age >= 17:
+    print("You are old enough to drive. Congratulations")
+else:
+    print("You can't Drive. Grow Up. Become older. Then you can drive.")
